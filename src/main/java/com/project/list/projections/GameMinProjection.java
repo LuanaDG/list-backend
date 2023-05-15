9b267609d5abf5e1d1260acc6effa6d85a84baeb
Luana Dela Giustina <luanadelagiustina@gmail.com>
2023-05-15 09:59:57 -0300

    review ok

--- src/main/java/com/project/list/projections/GameMinProjection.java
+++ src/main/java/com/project/list/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.project.list.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
